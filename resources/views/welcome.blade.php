2377063d8c63623628305c1a46cf81c26498a2e9
Samar <shuhrat92hod@gmail.com>
2021-12-28 09:22:26 +0500

    Title name changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 @section('header') 
     <div id="intro" class="p-3 text-center bg-light">
-        <h1 class="mb-2 ">Welcome to my personal Blog</h1>     
+        <h1 class="mb-2 ">Welcome to my personal Blog!</h1>     
     </div>
 @endsection
 
